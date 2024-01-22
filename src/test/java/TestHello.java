41ad50074b54c709726ef28421c787a96cf4c566
charles01a1 <aina1@ualberta.ca>
2024-01-22 13:24:14 -0700

    second test

--- src/test/java/TestHello.java
+++ src/test/java/TestHello.java
@@ -1,13 +1,13 @@
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 public class TestHello {
 
     @Test
     public void testFoo() {
         int result = 5 + 5 * 2;
-        assertEquals(result, 15);
+        assertEquals(result, 13);
     }
 
 }
