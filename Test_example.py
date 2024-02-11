cce2eac161288101fb84a7aac6acfd44889b7985
charles01a1 <aina1@ualberta.ca>
2024-02-10 22:21:01 -0700

    help

---
+++ Test_example.py
@@ -0,0 +1,22 @@
+import requests 
+import json
+
+def test_code() :
+    r = requests. get('http://localhost:8085')
+    json_response = r. json()
+    assert json_response ["code"] == 404
+    
+def test_message():
+    r = requests.get('http://localhost:8085')
+    json_response = r.json()
+    assert json_response["message"] == "HTTP 404 Not Found"
+    
+def test_correct_code() :
+    r = requests. get('http://localhost:8085/hello-world')
+    json_response = r. json()
+    assert json_response ["id"] == 2
+    
+def test_correct_message():
+    r = requests.get('http://localhost:8085/hello-world')
+    json_response = r.json()
+    assert json_response["content"] == "Hello, Stranger!"
